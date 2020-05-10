a7c1de304374a81381ccc95ac06fef1046c34316
Nurdan Ay <nurdan.ay@uzh.ch>
2020-05-10 11:32:52 +0200

    changed github for joeynmt

--- scripts/download_install_packages.sh
+++ scripts/download_install_packages.sh
@@ -10,7 +10,7 @@ echo "Make sure this script is executed AFTER you have activated a virtualenv"
 
 # install joeynmt
 
-git clone https://github.com/bricksdont/joeynmt.git $tools/joeynmt
+git clone https://github.com/nurdanay/joeynmt/tree/factors_complete $tools/joeynmt
 
 # if necessary: first check out the correct branch:
 
